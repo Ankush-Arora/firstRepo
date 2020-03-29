729a55c7c44b8453f602bcb4247c98cb2147b2e1
Ankush-Arora <62167353+Ankush-Arora@users.noreply.github.com>
2020-03-29 23:47:00 +0530

    Remove Error

--- Program.java
+++ Program.java
@@ -6,7 +6,7 @@ public static void main(String[] args)
   Scanner sc=new Scanner(System.in);
   System.out.println("Enter the value of a");
   int a=sc.nextInt();
-  System.out.println(Enter the valus of b );
+  System.out.println("Enter the valus of b ");
   int b=sc.nextInt();
   int result=a+b;
   System.out.println("The sum of two number is ="+result);
